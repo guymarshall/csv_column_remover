4dd0dbe521b2fad322c11b2241377dba55e90840
Guy Marshall <guymarshall9@gmail.com>
2026-01-30 21:48:00 +0000

    WIP: Search filename for headers, then prompt user for chosen header, then print whether it exists or not

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use clap::Parser;
+use csv::Reader;
+use promptput::input;
+use std::{error::Error, fs::File, path::Path};
+
+/// CSV - delete columns entirely based on first row (column headings) and print progress
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the file to edit
+    #[arg(short, long)]
+    filename: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Args = Args::parse();
+
+    let filename: String = args.filename;
+
+    if !Path::new(&filename).exists() {
+        panic!("File does not exist.");
+    }
+
+    let mut reader: Reader<File> = Reader::from_path(filename)?;
+    let headers: Vec<String> = reader
+        .headers()?
+        .iter()
+        .map(|header: &str| header.to_string())
+        .collect();
+    println!("{:?}", headers);
+
+    let header_choice: String = input("Which header would you like to remove?");
+    match headers
+        .iter()
+        .any(|header: &String| header.eq(&header_choice))
+    {
+        true => println!("Exists!"),
+        false => eprintln!("DOES NOT EXIST!"),
+    };
+    // work out index of header_choice in headers
+    // calculate lines of file
+    // for each line of file (ignoring first line (headers)):
+    // GOTO index based on ',' characters
+    // remove all items in that specific column
+
+    Ok(())
+}
